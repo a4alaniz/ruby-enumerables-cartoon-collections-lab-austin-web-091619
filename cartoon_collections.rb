d9888461bdffa28694cc48db5ddd8c28f3b75ce5
Angel Alanis <a4alaniz@gmail.com>
2019-09-09 22:46:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |elem, i| 
+    iplusone = i + 1
+  puts "#{iplusone} #{elem}"
+ end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(food)
+  food.collect {|food|
   # Your code here
 end
 
