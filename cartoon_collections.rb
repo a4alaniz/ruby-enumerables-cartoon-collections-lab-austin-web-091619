64540fa92fb3fd829fa5c12c072165b615ddeca2
Angel Alanis <a4alaniz@gmail.com>
2019-09-09 23:25:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(food)
 end
 
 def long_planeteer_calls(array)
-   array.one? {|item| item.length > 4}
+   array.any? {|item| item.length > 4}
 end
 
 def find_the_cheese# code an argument here
